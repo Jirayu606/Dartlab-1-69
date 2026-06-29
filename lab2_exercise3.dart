bda863e09ae67d8597d4eb0ed322509d7f4f19cf
Jirayu606 <beam12345a@gmail.com>
2026-06-29 12:09:11 +0700

    first commit

---
+++ lab2_exercise3.dart
@@ -0,0 +1,18 @@
+void main() {
+  List<String> subject = ['Math','Eng','History'];
+  print(subject.length);
+  print(subject[0]);
+  
+  subject.add('Game');
+  print(subject);
+  
+  Map<String, int> studentScores = {
+    'Math' : 40,
+    'Eng' : 80,
+    'History' : 40,
+    };
+  print(studentScores['Math']);
+  studentScores['Game'] = 90;
+  print(studentScores.keys);
+  print(studentScores.values);
+  }
